cd1404140762a6861186c069a684b9444b7348c5
Hzlip <3260106265@zju.edu.com>
2026-09-16 19:02:47 +0800

    add .gitignore

--- helloworld.c
+++ helloworld.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 
-int main() {
-    printf("Hello, GitHub!\n");
+int main() 
+{
+    // /n==newline
+    // decimal
+    //十六进制 0123456789abcdef 0x
+    //八进制 0
+    //大小写有区别喵
+    int a = 341;
+    printf("Hello, GitHub!(%d)\n",a);
+    int b = 0xabcd;
+    printf("%x\n",b);
+    a += 3;
+    printf("a=%d",a);
+    scanf("%d%d",&a,&b);
+    // \n咋加啊不会
     return 0;
 }
